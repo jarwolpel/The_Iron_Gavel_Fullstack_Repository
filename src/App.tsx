4138cd2240dac84b4e7b9c2eac32ccf974f61395
jpelletier5@rrc.ca <jaredwpelletier@gmail.com>
2026-01-20 10:05:37 -0600

    created pages folder. moved battlescreen compnent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import { Routes, Route, Router} from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import './App.css'
 
-import EventsLog, { appendEventToLog } from './components/BattleScreen/BattleScreenEventsLog/EventsLog'
 import { Layout } from './components/common/layout/layout';
-import BattleScreen from './components/BattleScreen/BattleScreen';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
