9a501357f53f8b0705d16b0e354fd64a8cc4d7e3
rrc_f2024_mkenyon <mkenyon@rrc.ca>
2026-01-27 08:58:20 -0600

    Fix route structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-          <Route index element={
+          <Route element={
             <LoginPage/>
           }/>
       </Route>
