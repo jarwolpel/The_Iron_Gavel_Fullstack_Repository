be68a4c6054302f27143fa43f36229c478a5623a
rrc_f2024_mkenyon <mkenyon@rrc.ca>
2026-02-06 11:08:27 -0600

    Fix: Updated some styling to fix nav button positioning
    Fix: Imported MenuBox for easier styling and organization

--- src/components/pages/FavoriteCharacter.tsx
+++ src/components/pages/FavoriteCharacter.tsx
@@ -3,6 +3,7 @@ import type { Character } from "../../../data/characterList";
 import { Search } from "../common/CharacterFavorite/search";
 import { CharacterListDisplay } from "../common/CharacterFavorite/CharacterListDisplay";
 import { characters as initialCharacters } from "../../../data/characterList";
+import { MenuBox } from "../common/menu-box/menuBox";
 
 export function Favorites() {
   const [searchValue, setSearchValue] = useState("");
@@ -14,38 +15,19 @@ export function Favorites() {
   );
 
   return (
-    <div
-        style={{
-            width: "100%",
-            minHeight: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "flex-start",
-            padding: "40px 0",}}>
-                <div
-                    style={{
-                        width: "90%",
-                        maxWidth: "1200px",
-                        padding: "24px",
-                        marginTop: "280px",
-                        backgroundColor: "rgba(20, 20, 20, 0.9)",
-                        border: "2px solid #ceb07a",
-                        borderRadius: "12px",
-                        boxShadow: "0 4px 12px rgba(0,0,0,0.3)",
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "20px",
-                        color: "white",
-                        pointerEvents: "auto",}}>
-            <h2>Pick Your Favorite Characters</h2>
-            <Search searchValue={searchValue} handleSearchChange={setSearchValue} />
+    <MenuBox>
+      <div>
+        <div>
+          <h2>Pick Your Favorite Characters</h2>
+          <Search searchValue={searchValue} handleSearchChange={setSearchValue} />
             {searchValue.trim() !== "" && (
-                <CharacterListDisplay
+              <CharacterListDisplay
                 characters={filteredCharacters}
                 updateCharacters={setCharacters}/>
-            )}
+              )}
         </div>
-    </div>
+      </div>
+    </MenuBox>
   );
 }
 
