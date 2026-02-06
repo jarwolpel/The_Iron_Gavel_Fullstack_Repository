59994a0ebaf3c11a36fcdcf7adf923f5b091bded
rrc_f2024_mkenyon <mkenyon@rrc.ca>
2026-02-06 15:57:40 -0600

    Fix: Added working hamburger menu and some responsive design

--- src/components/common/layout/nav/Nav.tsx
+++ src/components/common/layout/nav/Nav.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { useState, useRef, useEffect } from "react";
 import "./nav.css"
 import type { Credentials } from "../../../../types/userCredentials";
 
@@ -9,47 +10,104 @@ export function Nav(
         loggedInUser: Credentials
     }
 ) {
+    const [isVisible, setIsVisible] = useState(false);
+    const sidebarRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (
+                sidebarRef.current &&
+                !sidebarRef.current.contains(event.target as Node)
+            ) {
+                setIsVisible(false)
+            }
+        }
+
+        if (isVisible) {
+            document.addEventListener("mousedown", handleClickOutside)
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+
+    }, [isVisible])
+
     return(
-        <nav>
-            <ul className="sidebar">
-                <li><a className="cinzel-decorative-bold"><h1>Battle Simulator</h1></a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Battles</a></li>
-                <li><a href="#">Characters</a></li>
-                <li><a href="#">Favorites</a></li>
-                <li><a href="#"><button>Login</button></a></li>
-            </ul>
+        <nav className="roboto-heading">
+            {/* Sidebar Links here */}
+            {isVisible && (
+                <div className="sidebar" ref={sidebarRef}>
+                    <div className="sidebar-links">
+                        <div 
+                            className="menu item"
+                            onClick={() => setIsVisible(!isVisible)}
+                        >
+                            <a href="#"><img src="./src/assets/close.svg" alt="hamburger menu"></img></a>
+                        </div>
+                        <div className="item">
+                            <NavLink to="/" end>Home</NavLink>
+                        </div>
+                        <div className="item">
+                                <NavLink to="/test-character-select2">Character Select</NavLink>
+                        </div>
+                        <div className="item">
+                            <NavLink to="/battles/battle-screen">Battle Screen</NavLink>
+                        </div>
+                        <div className="item">
+                            <NavLink to="/Favorites">Favorite</NavLink>
+                        </div>
+                        <div className="item">
+                            <NavLink to="/accounts/login"><button>Login</button></NavLink>
+                        </div>
+                        <div className="item profile">
+                            <a href="#">
+                                <img src="./src/assets/account_circle.svg" alt="profile pic"></img>
+                                <p>{loggedInUser.username}</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {/* Main Nav Links here */}
             <div className="container">
                 <div className="title">
                     <a className="cinzel-decorative-bold"><h1>Battle Simulator</h1></a>
                 </div>
                 <div className="links">
-                    <div className="item">
-                        <NavLink to="/" end>Home</NavLink>    
+                    <div className="nav-links">
+                        <div className="item">
+                            <NavLink to="/" end>Home</NavLink>    
+                        </div>
+                        <div className="item">
+                            <NavLink to="/test-character-select2">Character Select</NavLink>
+                        </div>
+                        <div className="item">
+                            <NavLink to="/battles/battle-screen">Battle Screen</NavLink>
+                        </div>
+                        <div className="item">
+                            <NavLink to="/Favorites">Favorite</NavLink>
+                        </div>
+                        <div className="item">
+                            <NavLink to="/accounts/login"><button>Login</button></NavLink>
+        
+                        </div>
+                        <div className="item profile">
+                            <a href="#">
+                                <img className="profile-bits" src="./src/assets/account_circle.svg" alt="profile pic"></img>
+                                <p className="profile-bits">{loggedInUser.username}</p>
+                            </a>
+                        </div>
                     </div>
-                    <div className="item">
-                        <NavLink to="/test-character-select2">Character Select</NavLink>
-                    </div>
-                    <div className="item">
-                        <NavLink to="/battles/battle-screen">Battle Screen</NavLink>
-                    </div>
-                    <div className="item">
-                        <NavLink to="/Favorites">Favorite</NavLink>
-                    </div>
-                    <div className="item">
-                        <NavLink to="/accounts/login"><button>Login</button></NavLink>
-    
-                    </div>
-                    <div className="item profile">
-                        <a href="#">
-                            <img src="./src/assets/account_circle.svg" alt="profile pic"></img>
-                            <p>{loggedInUser.username}</p>
-                        </a>
-                    </div>
-                    <div className="item">
+
+                    {/* hamburger menu */}
+                    <div 
+                    className="menu item"
+                    onClick={() => setIsVisible(!isVisible)}
+                    >
                         <a href="#"><img src="./src/assets/menu.svg" alt="hamburger menu"></img></a>
                     </div>
-                    
                 </div>
             </div>
         </nav>
