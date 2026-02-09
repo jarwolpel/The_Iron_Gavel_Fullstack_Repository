ae8970f3c492db3f142a9c905803491d7aaae2fd
jpelletier5@rrc.ca <jaredwpelletier@gmail.com>
2026-02-09 09:42:10 -0600

    Refactored user credentials and added mock API service

--- src/apis/characterData.ts
+++ src/apis/characterData.ts
@@ -1,4 +1,67 @@
-// This will hold some character data
-const charData = "Joe Guy"
+import khan from "./characterImages/Genghis_Khan.jpeg"
+import nikola from "./characterImages/Nikola-Tesla.jpg"
+import thomas from "./characterImages/Thomas_Edison.jpg"
+import chief from "./characterImages/Master-Chief.jpeg"
+import healthIcon from "../assets/icons/health.png"
+import shieldIcon from "../assets/icons/shield.png"
+import swordIcon from "../assets/icons/sword.png"
 
-console.log(charData)
+export interface Character{
+    id: string;
+    name: string;
+    health: number;
+    damage: number;
+    armor: number;
+    image: string;
+    healthimg: string;
+    shieldimg:string;
+    swordimg:string;
+    isFavorite?:boolean;
+}
+
+export const characters: Character[] = [
+    {   
+        id: "Character-1",
+        name: "Nikola Tesla",
+        health: 100,
+        damage: 30,
+        armor: 15,
+        image: nikola, 
+        healthimg: healthIcon,
+        shieldimg: shieldIcon,
+        swordimg: swordIcon,
+    },
+    {   
+        id: "Character-2",
+        name: "Thomas Edison",
+        health: 100,
+        damage: 65,
+        armor: 50,
+        image: thomas,
+        healthimg: healthIcon,
+        shieldimg: shieldIcon,
+        swordimg: swordIcon,
+    },
+    {   
+        id: "Character-3",
+        name: "Ganghis Khan",
+        health: 100,
+        damage: 75,
+        armor: 30,
+        image: khan,
+        healthimg: healthIcon,
+        shieldimg: shieldIcon,
+        swordimg: swordIcon,
+    },
+    {   
+        id: "Character-4",
+        name: "Master Chief",
+        health: 9000,
+        damage: 500,
+        armor: 7500,
+        image: chief,
+        healthimg: healthIcon,
+        shieldimg: shieldIcon,
+        swordimg: swordIcon,
+    }
+];
