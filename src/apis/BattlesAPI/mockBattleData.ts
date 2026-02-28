a1e9c0f312628ba995dfa5e167d171165775e7ac
rrc_f2024_mkenyon <mkenyon@rrc.ca>
2026-02-27 18:57:26 -0600

    Fix: mockBattleData now had 10 examples

--- src/apis/BattlesAPI/mockBattleData.ts
+++ src/apis/BattlesAPI/mockBattleData.ts
@@ -28,5 +28,68 @@ export const initialBattles: Battle[] = [
             characterRepository.findOrThrow("Genghis Khan"), 
             characterRepository.findOrThrow("Master Chief")
         ]
-    }
+    },
+    {
+        id: "4",
+        name: "Battle of the Oasis",
+        description: "Read Player 1",
+        characters: [
+            characterRepository.findOrThrow("Genghis Khan"), 
+            characterRepository.findOrThrow("Master Chief")
+        ]
+    },
+    {
+        id: "5",
+        name: "Kings Row",
+        description: "A classic Rienhardt mirror match",
+        characters: [
+            characterRepository.findOrThrow("Reinhardt"), 
+            characterRepository.findOrThrow("Reinhardt")
+        ]
+    },
+    {
+        id: "6",
+        name: "Numbani",
+        description: "Dive vs Poke",
+        characters: [
+            characterRepository.findOrThrow("Sigma"), 
+            characterRepository.findOrThrow("Winston")
+        ]
+    },
+    {
+        id: "7",
+        name: "Route 66",
+        description: "A Spaghetti Western",
+        characters: [
+            characterRepository.findOrThrow("Cole Cassidy"), 
+            characterRepository.findOrThrow("Elizabeth Ashe")
+        ]
+    },
+    {
+        id: "8",
+        name: "Ilios Well",
+        description: "THE CAT HAS A JETPACK",
+        characters: [
+            characterRepository.findOrThrow("Jetpack Cat"), 
+            characterRepository.findOrThrow("Roadhog")
+        ]
+    },
+    {
+        id: "9",
+        name: "Lijiang Tower",
+        description: "The Dragon meets The Blizzard",
+        characters: [
+            characterRepository.findOrThrow("Genji"), 
+            characterRepository.findOrThrow("Mei")
+        ]
+    },
+    {
+        id: "10",
+        name: "Mario's Galaxy",
+        description: "yAHOOOOoooooo",
+        characters: [
+            characterRepository.findOrThrow("Mario"), 
+            characterRepository.findOrThrow("Turtle")
+        ]
+    },
 ];
