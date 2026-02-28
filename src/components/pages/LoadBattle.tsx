85f3ec44130da63e529e28330ad5a58863be7aee
rrc_f2024_mkenyon <mkenyon@rrc.ca>
2026-02-28 16:37:49 -0600

    Fix: Reformatted Battle History
    Fix: made the main menu box responsive

--- src/components/pages/LoadBattle.tsx
+++ src/components/pages/LoadBattle.tsx
@@ -1,3 +1,5 @@
+import type { FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { MenuBox } from "../common/menu-box/menuBox"
 import { BattleSelect } from "../common/BattleHistory/BattleSelect"
 import type { Battle } from "../../types/battle";
@@ -7,16 +9,25 @@ interface LoadBattleProps {
 }
 
 function LoadBattle({ battles }: LoadBattleProps) {
+  const navigate = useNavigate();
+    
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        navigate("/battles/battle-screen"); 
+    };
+
   return (
     <MenuBox>
-      <div>
-        <h1>Battle History</h1>
-        <div className="battle-list">
-          {battles.map((battle) => (
-            <BattleSelect key={battle.id} battle={battle} />
-          ))}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h1>Battle History</h1>
+          <div className="battle-list">
+            {battles.map((battle) => (
+              <BattleSelect key={battle.id} battle={battle} />
+            ))}
+          </div>
         </div>
-      </div>
+      </form>
     </MenuBox>
   );
 }
