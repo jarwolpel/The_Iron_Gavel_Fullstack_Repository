cd6a26912583b14a2d6f81e4d99c2c59edf2b86b
jpelletier5@rrc.ca <jaredwpelletier@gmail.com>
2026-03-16 17:30:50 -0500

    Added missing save file

--- apps/backend/src/api/v1/controllers/credentialController.ts
+++ apps/backend/src/api/v1/controllers/credentialController.ts
@@ -3,3 +3,4 @@ import * as credentialService from "../services/credentialService";
 import { successResponse } from "../models/responseModel";
 // import { Credential } from "../../../../types/userCredentials";
 import { FrontEndCredential } from "@shared/types/userCredentials";
+
