39e8801c992b05dd1ff8fad7a73ad294b214363e
jpelletier5@rrc.ca <jaredwpelletier@gmail.com>
2026-03-23 09:43:00 -0500

    Added missing vite.config.ts code

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//   ],
+// })
+/// 
+/// <reference types="vite" />
+
+import {  type ViteUserConfig, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// This configuration is required for making requests to a local server
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()] as ViteUserConfig["plugins"],
+  test: {
+    environment: 'jsdom'
+  }
 })
