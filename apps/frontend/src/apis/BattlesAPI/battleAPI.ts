1a26381bbd91437b507396b73d008c031e58ca26
rrc_f2024_mkenyon <mkenyon@rrc.ca>
2026-03-23 23:38:25 -0500

    Fix: Updated frontend battleAPI to query the backend

--- apps/frontend/src/apis/BattlesAPI/battleAPI.ts
+++ apps/frontend/src/apis/BattlesAPI/battleAPI.ts
@@ -1,18 +1,77 @@
 import type { Battle } from "../../types/battle";
 import { initialBattles } from "./mockBattleData";
 
-export function fetchBattles(): Battle[] {
-    return initialBattles;
+type BattlesResponseJSON = {message: String, data: Battle[]};
+type BattleResponseJSON = {message: String, data: Battle};
+
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/v1`;
+const BATTLE_ENDPOINT = "/credential";
+
+export async function fetchBattles(): Promise<Battle[]> {
+    const battleResponse: Response = await fetch(
+        `${BASE_URL}${BATTLE_ENDPOINT}`
+    );
+    
+    if(!battleResponse.ok) {
+        throw new Error("Failed to fetch battles");
+    }
+
+    const json: BattlesResponseJSON = await battleResponse.json();
+    return json.data;
 }
 
-export function getBattleByID(battleID: string): Battle {
-    const battle = initialBattles.find(id => id.id === battleID);
+export async function getBattleByID(battleID: string): Promise<Battle> {
+    // const battle = initialBattles.find(id => id.id === battleID);
+    const battleResponse: Response = await fetch(
+        `${BASE_URL}${BATTLE_ENDPOINT}/${battleID}`
+    )
 
-    if(!battle) {
+    if(!battleResponse.ok) {
         throw new Error(`Failed to find battle with ID ${battleID}`);
     }
 
-    return battle;
+    const json: BattleResponseJSON = await battleResponse.json();
+    return json.data;
+}
+
+export async function createBattleData(battle: Battle) {
+    const createResponse: Response = await fetch(
+        `${BASE_URL}${BATTLE_ENDPOINT}`,
+        {
+            method: "POST",
+            body: JSON.stringify({...battle}),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }     
+    );
+
+    if(!createResponse.ok) {
+        throw new Error(`Failed to create battle`);
+    }
+
+    const json: BattleResponseJSON = await createResponse.json();
+    return json.data;
+}
+
+export async function updateBattleData(battle: Battle) {
+    const createResponse: Response = await fetch(
+        `${BASE_URL}${BATTLE_ENDPOINT}/${battle.id}`,
+        {
+            method: "PUT",
+            body: JSON.stringify({...battle}),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }     
+    );
+
+    if(!createResponse.ok) {
+        throw new Error(`Failed to update battle with id ${battle.id}`);
+    }
+
+    const json: BattleResponseJSON = await createResponse.json();
+    return json.data;
 }
 
 export async function updateCharacterData(battle: Battle) {
