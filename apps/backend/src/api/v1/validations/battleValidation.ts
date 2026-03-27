dbcc53e1abfd6905c9b75cd83da6ac24593b8f03
rrc_f2024_mkenyon <mkenyon@rrc.ca>
2026-03-27 16:31:08 -0500

    Fix: corrected battle validation to accept the array of characters

--- apps/backend/src/api/v1/validations/battleValidation.ts
+++ apps/backend/src/api/v1/validations/battleValidation.ts
@@ -1,7 +1,5 @@
 import Joi, { ObjectSchema } from "joi";
-
-// define the correct shape of a term object received in JSON
-// Require a title and definition string at minimum
+ 
 export const battleSchema: ObjectSchema = Joi.object({
     name: Joi.string().required().messages({
         "any.required": "Name is required",
@@ -11,8 +9,8 @@ export const battleSchema: ObjectSchema = Joi.object({
         "any.required": "Description is required",
         "string.empty": "Description cannot be empty"
     }),
-    characters: Joi.string().required().messages({
+    characters: Joi.array().items(Joi.string()).required().messages({
         "any.required": "Exactly two characters are required",
-        "string.empty": "Please select two characters"
+        "array.base": "Characters must be an array of strings"
     }),
 });
